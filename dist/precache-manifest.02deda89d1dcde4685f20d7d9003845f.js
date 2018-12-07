3d414397a4954cd91efc2464380f4d9ae2dd7f6c
1172716211 <1172716211@qq.com>
2018-12-07 20:28:05 +0800

    123

---
+++ dist/precache-manifest.02deda89d1dcde4685f20d7d9003845f.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "6423c0681bfb3358d5c684c9b998e412",
+    "url": "/music/img/iconlist.png"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/music/robots.txt"
+  },
+  {
+    "revision": "b0fa37cfa96c72badbe7d102a0800f54",
+    "url": "/music/img/listen.svg"
+  },
+  {
+    "revision": "4bbe2cdae83697a58143",
+    "url": "/music/js/chunk-vendors.45070c6a.js"
+  },
+  {
+    "revision": "a6af3ec1d21986b9757d96e8601b8831",
+    "url": "/music/img/moreup.png"
+  },
+  {
+    "revision": "b7b6cf740a22b0aa7bd9be54c30ebbdb",
+    "url": "/music/img/right.b7b6cf74.svg"
+  },
+  {
+    "revision": "d8a7becce353c96026179cf0ab8bae37",
+    "url": "/music/img/search.d8a7becc.svg"
+  },
+  {
+    "revision": "98226743c9018e5154caa8e5d38d0bf6",
+    "url": "/music/index.html"
+  },
+  {
+    "revision": "2105b609ee8b420d99e6d799a138e420",
+    "url": "/music/img/vip.png"
+  },
+  {
+    "revision": "d8a7becce353c96026179cf0ab8bae37",
+    "url": "/music/img/search.svg"
+  },
+  {
+    "revision": "cc0f31ce8ff0d0083397",
+    "url": "/music/js/app.32db0e43.js"
+  },
+  {
+    "revision": "b7b6cf740a22b0aa7bd9be54c30ebbdb",
+    "url": "/music/img/right.svg"
+  },
+  {
+    "revision": "5b38bbd2f23cce9a05b7aa123f665ba7",
+    "url": "/music/img/right.png"
+  },
+  {
+    "revision": "5c9abc4a022ec256b1d0b7dd12f4e0b0",
+    "url": "/music/img/play.png"
+  },
+  {
+    "revision": "b0fa37cfa96c72badbe7d102a0800f54",
+    "url": "/music/img/listen.b0fa37cf.svg"
+  },
+  {
+    "revision": "d506e88f1f436d6b2aca6a5c231a6046",
+    "url": "/music/img/down.png"
+  },
+  {
+    "revision": "5207a28c3767992ca4bb6d4887c74880",
+    "url": "/music/img/control.png"
+  },
+  {
+    "revision": "5207a28c3767992ca4bb6d4887c74880",
+    "url": "/music/img/control.5207a28c.png"
+  },
+  {
+    "revision": "85a01719f742b57f93727c92a9b24792",
+    "url": "/music/img/Up.png"
+  },
+  {
+    "revision": "cc0f31ce8ff0d0083397",
+    "url": "/music/css/app.a5e5be1b.css"
+  }
+];
